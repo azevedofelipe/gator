a2c5898dc4ffd45127a7dbeebf80078fed8c5b90
Felipe <felipe.azevedo@dna.tec.br>
2025-05-02 01:54:42 -0300

    Integrate database, create user commands

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/azevedofelipe/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -11,11 +17,68 @@ func handlerLogin(s *state, cmd command) error {
 
 	username := cmd.Args[0]
 
+	if _, err := s.db.GetUser(context.Background(), username); err != nil {
+		return fmt.Errorf("User doesnt exist")
+	}
+
 	err := s.cfg.SetUser(username)
 	if err != nil {
 		return fmt.Errorf("Couldnt set current user: %w", err)
 	}
 
 	fmt.Println("User switched succesfully")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("name is required")
+	}
+
+	name := cmd.Args[0]
+
+	args := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	}
+
+	_, err := s.db.CreateUser(context.Background(), args)
+	if err != nil {
+		return err
+	}
+
+	s.cfg.SetUser(name)
+
+	fmt.Printf("User %s was created succesfully\n", name)
+	log.Printf("User %s was created\n", name)
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("User table reset")
+	return nil
+}
+
+func handlerUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("Couldnt list users: %w", err)
+	}
+
+	for _, user := range users {
+		if user.Name == s.cfg.User {
+			fmt.Printf("* %s (current)\n", user.Name)
+			continue
+		}
+
+		fmt.Printf("* %s", user.Name)
+	}
 	return nil
 }
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/azevedofelipe/gator/internal/config"
+	"github.com/azevedofelipe/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -17,11 +21,22 @@ func main() {
 		log.Fatalf("Error reading config file")
 	}
 
+	db, err := sql.Open("postgres", gatorConfig.DBUrl)
+	if err != nil {
+		log.Fatalf("Error connecting to db: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
 	currentState := state{}
 	currentState.cfg = &gatorConfig
+	currentState.db = dbQueries
 
 	commandList := commands{commandList: make(map[string]func(*state, command) error)}
 	commandList.register("login", handlerLogin)
+	commandList.register("register", handlerRegister)
+	commandList.register("reset", handlerReset)
+	commandList.register("users", handlerUsers)
 
 	args := os.Args
 	if len(args) < 2 {
